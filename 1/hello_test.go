625abf130381d4cd11bc804fd870caaa6a972571
Andrew Hillier <andrew.hillier@hellofresh.com>
2022-04-14 11:15:32 +0200

    add spanish RG

--- 1/hello_test.go
+++ 1/hello_test.go
@@ -11,13 +11,18 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Andrew")
+		got := Hello("Andrew", "")
 		want := "Hello, Andrew"
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Andrew", "Spanish")
+		want := "Hola, Andrew"
 		assertCorrectMessage(t, got, want)
 	})
 }
