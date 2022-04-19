4175b3888f4358c1e21d65c21dd55cbf9b61e7b3
Andrew Hillier <andrew.hillier@hellofresh.com>
2022-04-19 22:03:03 +0200

    iteration4: add spy

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -2,27 +2,45 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
 func TestCountdown(t *testing.T) {
-	buffer := &bytes.Buffer{}
-	spySleeper := &SpySleeper{}
+	t.Run("prints 3 to Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
 
-	Countdown(buffer, spySleeper)
+		Countdown(buffer, &SpyCountdownOperations{})
 
-	got := buffer.String()
-	want := `3
-2
-1
-Go!
-`
+		got := buffer.String()
+		want := `3
+	2
+	1
+	Go!
+	`
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("sleep before every print", func(t *testing.T) {
+		spySleepPrinter := &SpyCountdownOperations{}
 
-	if spySleeper.Calls != 4 {
-		t.Errorf("not enough calls to sleeper, want 4 got %d", spySleeper.Calls)
-	}
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		want := []string{
+			"sleep",
+			"write",
+			"sleep",
+			"write",
+			"sleep",
+			"write",
+			"sleep",
+			"write",
+		}
+
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
+		}
+	})
 }
